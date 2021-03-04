bca12d921b6d35c5ef686536462e55698fd5d70b
Abraham935 <Abraham_935@hotmail.com>
2021-03-03 19:43:19 -0600

    Resolución de primer problema para act 2.2

--- Tarea 2/actividad2.2.scm
+++ Tarea 2/actividad2.2.scm
@@ -1,3 +1,13 @@
-;   Abraham Mendoza
+;   Abraham Mendoza A01274857
 ;   Aldo Degollado
 ;   Luis Alonso Martínez García A01636255
+
+;1. La funcion insert toma dos entradas: un numero n y una lista lst que contiene numeros en orden ascendente.
+;Devuelve una nueva lista con los mismos elementos de lst pero con n insertado en su lugar correspondiente.
+
+ (define (insert n lst)
+      (cond
+        ((null? lst) (list n))
+        ((>= (car lst) n) (cons n lst))
+        (else
+          (cons (car lst) (insert n (cdr lst))))))
